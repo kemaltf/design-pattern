9c4de911a741f031a59462b63a26adfcd56c2cbf
kemaltf <kemaltaufikf@gmail.com>
2023-10-02 14:46:43 +0800

    constructor pattern

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ class Car {
     }
 }
 
+// constructor pattern
+class SUV extends Car {
+    constructor(doors, engine, color){
+        super(doors, engine, color);
+        this.wheels = 4;
+    }
+}
+
 // Create 
-const civic = new Car(4, 'v6', 'grey')
-console.log(civic)
+const civic = new Car(4, 'v6', 'grey');
+const cx5 = new SUV(6,'v8','red');
+
+console.log(civic);
+console.log(cx5);
